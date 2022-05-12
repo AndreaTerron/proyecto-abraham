6ac9cfc6c919538a5dabe6b6613682b1be839ac0
AndreaTerron <37133810+AndreaTerron@users.noreply.github.com>
2022-05-12 09:41:50 +0200

    Actualizar main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 distance = 0
-servos.P0.set_angle(90)
+servos.P2.set_angle(90)
 
 def on_forever():
     global distance
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
+    basic.show_number(distance)
+    if distance < 15:
+        servos.P2.set_angle(0)
+        basic.pause(10)
+    else:
+        servos.P2.set_angle(90)
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 let distance = 0
-servos.P0.setAngle(90)
+servos.P2.setAngle(90)
 basic.forever(function on_forever() {
     
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+    basic.showNumber(distance)
+    if (distance < 15) {
+        servos.P2.setAngle(0)
+        basic.pause(10)
+    } else {
+        servos.P2.setAngle(90)
+    }
+    
 })
